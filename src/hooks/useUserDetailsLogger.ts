496a320af50c0e384ce71327871b2e9ae6eee54c
Jalpa Makvana <jhm@narola.email>
2024-06-11 15:12:06 +0530

    feat(Optimize) : Optimize the code for custom hook.

--- src/hooks/useUserDetailsLogger.ts
+++ src/hooks/useUserDetailsLogger.ts
@@ -1,15 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { User } from "../utils/common/person";
 import { useLogContext } from "../context/LoggingContext";
 
 const usePersonDetailsLogger = (user: User | null, currentTime: string) => {
+  const currentTimeData = useRef(currentTime);
   const { enableLogs } = useLogContext();
+
+  useEffect(() => {
+    currentTimeData.current = currentTime;
+  }, [currentTime]);
+
   useEffect(() => {
     if (enableLogs && user) {
       console.log("Person details:", user);
-      console.log("Current time:", currentTime);
+      console.log("Current time:", currentTimeData.current);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user, enableLogs]);
 };
 
